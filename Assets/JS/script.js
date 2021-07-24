435df5a137ee892aa37f05c5bbfaf59f52a89e2e
Lancelot <georgengo@georges-mbp.lan>
2021-07-24 10:48:50 -0700

    local storage features added

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,11 @@
+$(document).ready(function(){
+
 var currentDate = moment().format("dddd, MMMM Do, YYYY");
 var currentHour = moment().hour(); // Number
 var timeBlocks = $(".description");
 
-// Center the header and add a line to show current date Day written, Month, Day
-
-// Need blocks for hours between 9AM - 5 PM (3 columns: 1 for the time, 1 for task adding, 1 for saving down)
-
-// task adding block should be click into able for adding task
-
-// save block saves the task and persists through refresh
-
 //current hour turns block red, future hours are green and past hours gray out
 for (let i = 0; i < timeBlocks.length; i++) {
-    
   if (currentHour === +$(timeBlocks[i]).attr("id")) {
     $(timeBlocks[i]).addClass("present");
   } else if (currentHour > +$(timeBlocks[i]).attr("id")) {
@@ -22,12 +15,22 @@ for (let i = 0; i < timeBlocks.length; i++) {
   }
 }
 
-console.log(currentHour);
+// save block saves the task and persists through refresh
+$(".saveBtn").click(function () {
+  var siblings = $(this).siblings(".tasks").val();
+  var save = $(this).siblings(".tasks").attr("id");
+  localStorage.setItem("h" + save, siblings);
+});
+$("#hour9 .tasks").val(localStorage.getItem("h9"));
+$("#hour10 .tasks").val(localStorage.getItem("h10"));
+$("#hour11 .tasks").val(localStorage.getItem("h11"));
+$("#hour12 .tasks").val(localStorage.getItem("h12"));
+$("#hour13 .tasks").val(localStorage.getItem("h13"));
+$("#hour14 .tasks").val(localStorage.getItem("h14"));
+$("#hour15 .tasks").val(localStorage.getItem("h15"));
+$("#hour16 .tasks").val(localStorage.getItem("h16"));
+$("#hour17 .tasks").val(localStorage.getItem("h17"));
+
 // show current date Day written, Month, Day
 currentDay.innerHTML = currentDate;
-
-$(".saveBtn").click(function(){
-    console.log($(this));
-})
-
-//val()
+});
